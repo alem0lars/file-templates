9ea843d1db4e6e4b1c01902a16db1ce3dc79f6ae
Alessandro Molari <molari.alessandro@gmail.com>
2016-12-10 09:27:48 +0100

    update js template

---
+++ =template=.js
@@ -0,0 +1,16 @@
+/*
+ * @file %FFILE%
+ * @author Alessandro Molari (@alem0lars)
+ * @license %LICENSE%
+ * @version 1.0.0
+ * @since %HERE%
+ *
+ * @flow
+ */
+'use strict';
+
+class %CAMELCLASS% {
+
+}
+
+module.exports = %CAMELCLASS%;
